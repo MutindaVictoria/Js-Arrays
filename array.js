6441b09c0ef1ee5975d67f3a5de83042897384da
MutindaVictoria <victoriaamutinda@gmail.com>
2023-05-06 14:03:27 +0300

    arraysObjects

--- array.js
+++ array.js
@@ -11,30 +11,73 @@ console.log(Arrdesce(['a', 'c', 'z', 'f']));
 //Given an array of objects, each object representing a person with a name 
 //and age property, 
 //write a function that returns the sum of all people who are less than 18 years.
+function CheckAge(people) {
+    const result = [];
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].age >= 18) {
+            result.push(people[i].name);
+        }
+    }
+    return result;
+}
+  
 const people = [
-    { name: 'Alice', age: 17 },
-    { name: 'Eunice', age: 22 },
-    { name: 'Charlie', age: 14 },
-    { name: 'Max', age: 19 },
-  ];
-
-
+  { name: 'Alice', age: 17 },
+  { name: 'Eunice', age: 22 },
+  { name: 'Charlie', age: 14 },
+  { name: 'Max', age: 19 },
+];
+console.log(CheckAge(people));
 
 
   //Using JS functions and an array of numbers, 
   //return positive if an element within the array is positive,
    //negative if an element is negative, else zero
    function checkelements(num){
-    const num=12
-    if(num>0){
+    
+    if(num > 0){
         console.log("positive");
     }
-    else if (num<0){
+    else if (num < 0){
             console.log("negative")
-        }else{
+        }
+        else{
             console.log("zero");
         }
     
    }
-   console.log(12);
+   console.log(checkelements(12));
+  
+
+
+   ///Given an array of objects, where each object represents
+   // an employee with an id, name, and salary property, write a function that 
+   // returns a new array of employee objects sorted by their salary in ascending order.
+   function sortEmployees(employees) {
+    for (let i = 0; i < employees.length; i++) {
+      for (let j = i + 1; j < employees.length; j++) {
+        if (employees[i].salary > employees[j].salary) {
+          let x = employees[i];
+          employees[i] = employees[j];
+          employees[j] = x;
+        }
+      }
+    }
+    return employees;
+  }
+  
+  const employees = [
+    { name: 'John', salary: 100000 },
+    { name: 'Jane', salary: 200000 },
+    { name: 'Bob', salary: 300000},
+    { name: 'Alice', salary: 400000 }
+  ];
+  console.log(sortEmployees(employees));
+  
+  
+  
+  
+  
+  
+  
   
